b588a90b6da3ec1b039698d5df65f6188d271390
ytakahashi <yt01tc@gmail.com>
2021-05-16 09:28:01 +0900

    chore: bump deno/deno_std
    - deno 1.10
    - deno_std v0.96

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
 export { Select } from "https://deno.land/x/cliffy@v0.18.2/prompt/mod.ts";
 export { parseFlags } from "https://deno.land/x/cliffy@v0.18.2/flags/mod.ts";
-export { existsSync } from "https://deno.land/std@0.95.0/fs/mod.ts";
+export { existsSync } from "https://deno.land/std@0.96.0/fs/mod.ts";
--- test/deps.ts
+++ test/deps.ts
@@ -1,2 +1,2 @@
-export { assertEquals } from "https://deno.land/std@0.95.0/testing/asserts.ts";
-export { dirname, fromFileUrl } from "https://deno.land/std@0.95.0/path/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.96.0/testing/asserts.ts";
+export { dirname, fromFileUrl } from "https://deno.land/std@0.96.0/path/mod.ts";
