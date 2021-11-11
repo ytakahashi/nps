77708764c606efeb134fe51d2f018c6a6a41c7c1
ytakahashi <yt01tc@gmail.com>
2021-11-11 20:31:54 +0900

    chore: bump deps / deno 1.16.0

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
-export { Select } from "https://deno.land/x/cliffy@v0.19.2/prompt/mod.ts";
-export { parseFlags } from "https://deno.land/x/cliffy@v0.19.2/flags/mod.ts";
-export { existsSync } from "https://deno.land/std@0.99.0/fs/mod.ts";
+export { Select } from "https://deno.land/x/cliffy@v0.20.1/prompt/mod.ts";
+export { parseFlags } from "https://deno.land/x/cliffy@v0.20.1/flags/mod.ts";
+export { existsSync } from "https://deno.land/std@0.114.0/fs/mod.ts";
