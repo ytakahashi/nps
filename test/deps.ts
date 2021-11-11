5474229c4ebefff597f0e526b7bd6a99113f3a02
ytakahashi <yt01tc@gmail.com>
2021-11-11 20:34:04 +0900

    fmt

--- test/deps.ts
+++ test/deps.ts
@@ -1,2 +1,5 @@
 export { assertEquals } from "https://deno.land/std@0.114.0/testing/asserts.ts";
-export { dirname, fromFileUrl } from "https://deno.land/std@0.114.0/path/mod.ts";
+export {
+  dirname,
+  fromFileUrl,
+} from "https://deno.land/std@0.114.0/path/mod.ts";
