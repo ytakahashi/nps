a758fe6016c4186eee1ab9c95df663d734c9339f
ytakahashi <yt01tc@gmail.com>
2023-04-13 09:46:17 +0900

    chore: bump deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export { Select } from "https://deno.land/x/cliffy@v0.20.1/prompt/mod.ts";
-export { parseFlags } from "https://deno.land/x/cliffy@v0.20.1/flags/mod.ts";
+export { Select } from "https://deno.land/x/cliffy@v0.25.7/prompt/mod.ts";
+export { parseFlags } from "https://deno.land/x/cliffy@v0.25.7/flags/mod.ts";
