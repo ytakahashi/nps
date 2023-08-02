e7633a579b860be24323ca7f359651a31e65b386
ytakahashi <yt01tc@gmail.com>
2023-08-02 17:11:28 +0900

    chore: change assert import (#13)

--- test/deps.ts
+++ test/deps.ts
@@ -1,7 +1,5 @@
-export {
-  assertEquals,
-  assertRejects,
-} from "https://deno.land/std@0.196.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.196.0/assert/assert_equals.ts";
+export { assertRejects } from "https://deno.land/std@0.196.0/assert/assert_rejects.ts";
 export {
   dirname,
   fromFileUrl,
