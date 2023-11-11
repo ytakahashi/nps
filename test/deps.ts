e8f34cfaca9aa92782b2143f5db8640d50eaef18
ytakahashi <yt01tc@gmail.com>
2023-11-12 08:11:46 +0900

    chore: bump dependencies (#19)

--- test/deps.ts
+++ test/deps.ts
@@ -1,7 +1,7 @@
-export { assertEquals } from "https://deno.land/std@0.196.0/assert/assert_equals.ts";
-export { assertRejects } from "https://deno.land/std@0.196.0/assert/assert_rejects.ts";
+export { assertEquals } from "https://deno.land/std@0.206.0/assert/assert_equals.ts";
+export { assertRejects } from "https://deno.land/std@0.206.0/assert/assert_rejects.ts";
 export {
   dirname,
   fromFileUrl,
   SEP,
-} from "https://deno.land/std@0.196.0/path/mod.ts";
+} from "https://deno.land/std@0.206.0/path/mod.ts";
