88e4eb5b545d649b167a10c82c04968bb0a6934d
ytakahashi <yt01tc@gmail.com>
2023-12-22 21:08:29 +0900

    chore: bump deps

--- test/deps.ts
+++ test/deps.ts
@@ -1,7 +1,7 @@
-export { assertEquals } from "https://deno.land/std@0.206.0/assert/assert_equals.ts";
-export { assertRejects } from "https://deno.land/std@0.206.0/assert/assert_rejects.ts";
+export { assertEquals } from "https://deno.land/std@0.210.0/assert/assert_equals.ts";
+export { assertRejects } from "https://deno.land/std@0.210.0/assert/assert_rejects.ts";
 export {
   dirname,
   fromFileUrl,
   SEP,
-} from "https://deno.land/std@0.206.0/path/mod.ts";
+} from "https://deno.land/std@0.210.0/path/mod.ts";
