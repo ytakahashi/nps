0cbc33762371a90e15911d507899c4063758d6f6
ytakahashi <yt01tc@gmail.com>
2024-05-17 07:03:59 +0900

    update

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
 export { Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
 export { parseFlags } from "https://deno.land/x/cliffy@v1.0.0-rc.3/flags/mod.ts";
-export { $ } from "https://deno.land/x/dax@0.39.2/mod.ts";
+export { $ } from "@david/dax";
