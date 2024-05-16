01536ddc30e96861df5fa6e4e13308cfa7d0c2f6
ytakahashi <yt01tc@gmail.com>
2024-05-17 06:53:09 +0900

    chore: jsr

--- test/deps.ts
+++ test/deps.ts
@@ -1,7 +1,2 @@
-export { assertEquals } from "https://deno.land/std@0.219.1/assert/assert_equals.ts";
-export { assertRejects } from "https://deno.land/std@0.219.1/assert/assert_rejects.ts";
-export {
-  dirname,
-  fromFileUrl,
-  SEPARATOR,
-} from "https://deno.land/std@0.219.1/path/mod.ts";
+export { assertEquals, assertRejects } from "@std/assert";
+export { dirname, fromFileUrl, SEPARATOR } from "@std/path";
