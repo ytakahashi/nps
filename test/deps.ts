7f0da3ccb706045c473d97fbae21155e1921173b
ytakahashi <yt01tc@gmail.com>
2024-05-17 07:23:25 +0900

    chore: bump dependencies / change to jsr (#22)

--- test/deps.ts
+++ test/deps.ts
@@ -1,7 +1,2 @@
-export { assertEquals } from "https://deno.land/std@0.219.1/assert/assert_equals.ts";
-export { assertRejects } from "https://deno.land/std@0.219.1/assert/assert_rejects.ts";
-export {
-  dirname,
-  fromFileUrl,
-  SEPARATOR,
-} from "https://deno.land/std@0.219.1/path/mod.ts";
+export { assertEquals, assertRejects } from "jsr:@std/assert@^0.225.1";
+export { dirname, fromFileUrl, SEPARATOR } from "jsr:@std/path@^0.224.0";
