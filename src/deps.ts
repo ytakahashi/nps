cb08d9ae48db510c4761dbc13ab7c1f0e963531d
ytakahashi <yt01tc@gmail.com>
2024-07-13 16:30:27 +0900

    chore: bump dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
-export { Select } from "https://deno.land/x/cliffy@v1.0.0-rc.4/prompt/mod.ts";
-export { parseFlags } from "https://deno.land/x/cliffy@v1.0.0-rc.4/flags/mod.ts";
-export { $ } from "jsr:@david/dax@^0.41.0";
+export { Select } from "jsr:@cliffy/prompt@1.0.0-rc.5";
+export { parseFlags } from "jsr:@cliffy/flags@1.0.0-rc.5";
+export { $ } from "jsr:@david/dax@0.41.0";
