ee6dc3716dca254b5351f3a839114c038263db62
ytakahashi <yt01tc@gmail.com>
2024-09-29 14:50:02 +0900

    chore: bump dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
-export { Select } from "jsr:@cliffy/prompt@1.0.0-rc.5";
-export { parseFlags } from "jsr:@cliffy/flags@1.0.0-rc.5";
-export { $ } from "jsr:@david/dax@0.41.0";
+export { Select } from "jsr:@cliffy/prompt@1.0.0-rc.7";
+export { parseFlags } from "jsr:@cliffy/flags@1.0.0-rc.7";
+export { $ } from "jsr:@david/dax@0.42.0";
--- test/deps.ts
+++ test/deps.ts
@@ -1,2 +1,2 @@
-export { assertEquals, assertRejects } from "jsr:@std/assert@^1.0.0";
-export { dirname, fromFileUrl, SEPARATOR } from "jsr:@std/path@^0.224.0";
+export { assertEquals, assertRejects } from "jsr:@std/assert@^1.0.6";
+export { dirname, fromFileUrl, SEPARATOR } from "jsr:@std/path@^1.0.6";
