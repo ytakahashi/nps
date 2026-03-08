76a7850d1474ee84bba358cafe9ad7ef5a5c2da6
ytakahashi <yt01tc@gmail.com>
2026-03-09 08:58:36 +0900

    chore: bump dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
-export { Select } from "jsr:@cliffy/prompt@1.0.0-rc.8";
-export { parseFlags } from "jsr:@cliffy/flags@1.0.0-rc.8";
-export { $ } from "jsr:@david/dax@0.43.2";
+export { Select } from "jsr:@cliffy/prompt@1.0.0";
+export { parseFlags } from "jsr:@cliffy/flags@1.0.0";
+export { $ } from "jsr:@david/dax@0.45.0";
