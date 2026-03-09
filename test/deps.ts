9421e5eda6dce20ce91ef4089cea878562470610
ytakahashi <yt01tc@gmail.com>
2026-03-09 09:03:59 +0900

    chore: bump dependencies (#28)

--- test/deps.ts
+++ test/deps.ts
@@ -1,2 +1,2 @@
-export { assertEquals, assertRejects } from "jsr:@std/assert@^1.0.15";
-export { dirname, fromFileUrl, SEPARATOR } from "jsr:@std/path@^1.1.2";
+export { assertEquals, assertRejects } from "jsr:@std/assert@1.0.19";
+export { dirname, fromFileUrl, SEPARATOR } from "jsr:@std/path@1.1.4";
